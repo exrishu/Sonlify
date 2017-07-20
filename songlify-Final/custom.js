4b25183faa0b3a6c1368fb8ea19afb2b7630f6d2
Ayush Pandey <ayush.pandey10@outlook.com>
2017-07-20 19:09:23 +0530

    commit

---
+++ songlify-Final/custom.js
@@ -0,0 +1,11 @@
+ $('.welcome-screen button').on('click', function() {
+        var name = $('#name-input').val();
+        if (name.length > 3) {
+            var message = "Welcome, " + name;
+            $('.main .user-name').text(message);
+            $('.welcome-screen').addClass('hidden');
+            $('.main').removeClass('hidden');
+        } else {
+            $('#name-input').addClass('error');
+        }
+    });
